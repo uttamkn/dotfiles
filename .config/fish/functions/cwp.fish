c3eb80756b9af69936e6fd6f4181412155a75d69
uttamkn <uttamkn15@gmail.com>
2024-07-30 12:28:09 +0530

    Create a wallpapers directory

--- .config/fish/functions/cwp.fish
+++ .config/fish/functions/cwp.fish
@@ -3,5 +3,5 @@ function cwp
     set wallpaper $argv[1]
     wal -i $wallpaper
     killall swaybg
-    hyprctl hyprpaper wallpaper "HDMI-A-1, /home/utmkn/Pictures/$wallpaper"
+    hyprctl hyprpaper wallpaper "HDMI-A-1, /home/utmkn/Pictures/Wallpapers/$wallpaper"
 end
