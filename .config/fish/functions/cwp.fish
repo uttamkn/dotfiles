5e3c0d70dba21e9a6c507dfdef15fd4afa298356
uttamkn <uttamkn15@gmail.com>
2024-07-30 22:56:47 +0530

    Add swaync stuff

--- .config/fish/functions/cwp.fish
+++ .config/fish/functions/cwp.fish
@@ -3,6 +3,8 @@ function cwp
     set wallpaper $argv[1]
     wal -i $wallpaper
     killall swaybg
+    killall swaync
     hyprctl hyprpaper wallpaper "HDMI-A-1, /home/utmkn/Pictures/Wallpapers/$wallpaper"
     sh ~/.config/waybar/launch.sh
+    swaync &
 end
