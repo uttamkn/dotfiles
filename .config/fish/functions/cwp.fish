15c0b145321feaf2aab1a823b1ae011690bbf965
uttamkn <uttamkn15@gmail.com>
2024-08-06 12:31:41 +0530

    Changing wallpapers became 1000 times easier

--- .config/fish/functions/cwp.fish
+++ .config/fish/functions/cwp.fish
@@ -4,6 +4,8 @@ function cwp
     wal -i $wallpaper
     killall swaybg
     killall swaync
+    hyprctl hyprpaper unload all
+    hyprctl hyprpaper preload "/home/utmkn/Pictures/Wallpapers/$wallpaper"
     hyprctl hyprpaper wallpaper "HDMI-A-1, /home/utmkn/Pictures/Wallpapers/$wallpaper"
     sh ~/.config/waybar/launch.sh
     swaync &
