73c996cc07ade4dede969b2ca3464de2d70a5e4d
uttamkn <uttamkn15@gmail.com>
2024-11-03 23:58:23 +0530

    add vcpkg and remove tabs in vim

--- .config/nvim/lua/utmkn/plugins/bufferline.lua
+++ .config/nvim/lua/utmkn/plugins/bufferline.lua
@@ -2,10 +2,4 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
-	opts = {
-		options = {
-			mode = "tabs",
-			separator_style = "slant",
-		},
-	},
 }
