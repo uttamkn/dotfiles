5bfcc18e4a3243956c7920caa148e1cc4f6d2aa4
uttamkn <uttamkn15@gmail.com>
2025-01-26 08:43:20 +0530

    update cwp function and hyprstart to set different color scheme

--- .config/fish/functions/cwp.fish
+++ .config/fish/functions/cwp.fish
@@ -4,11 +4,12 @@ function cwp
     # Check if a second argument (color scheme image) is provided
     if set -q argv[2]
         set color_scheme $argv[2]
-        wal -i "$color_scheme" > /dev/null 2>&1
+        wal -i "$color_scheme" -n > /dev/null 2>&1
     else
-        wal -i "$wallpaper" > /dev/null 2>&1
+        wal -i "$wallpaper" -n > /dev/null 2>&1
     end
 
+    echo "\$swaywall = /home/utmkn/Pictures/Wallpapers/$wallpaper" >> ~/.cache/wallpaper.conf
     killall swaybg > /dev/null 2>&1
     killall swaync > /dev/null 2>&1
     swaybg -i "/home/utmkn/Pictures/Wallpapers/$wallpaper" > /dev/null 2>&1 &
