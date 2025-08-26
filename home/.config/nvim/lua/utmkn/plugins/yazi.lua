72437894fe7cbf4dd5fcad351dd11a083e27169a
uttamkn <uttamkn15@gmail.com>
2025-08-27 00:14:38 +0530

    switch to yazi nvim

---
+++ home/.config/nvim/lua/utmkn/plugins/yazi.lua
@@ -0,0 +1,33 @@
+return {
+	"mikavilpas/yazi.nvim",
+	version = "*", -- use the latest stable version
+	event = "VeryLazy",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim", lazy = true },
+	},
+	keys = {
+		{
+			"<leader>ee",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			-- Open in the current working directory
+			"<leader>ec",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+		{
+			"<c-y>",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+	opts = {
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+}
