e062df5a5d1216e2497372db159da4740d669ebe
uttamkn <uttamkn15@gmail.com>
2025-08-28 14:00:26 +0530

    remove plugin for linting

--- home/.config/nvim/lsp/lua_ls.lua
+++ home/.config/nvim/lsp/lua_ls.lua
@@ -18,7 +18,7 @@ return {
 	settings = {
 		Lua = {
 			diagnostics = {
-				disable = { "missing-parameters", "missing-fields" },
+				globals = { "vim" },
 			},
 			workspace = {
 				library = vim.api.nvim_get_runtime_file("", true),
