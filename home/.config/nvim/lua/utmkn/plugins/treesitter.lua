64fa34bf7234a62f1a90d2547a30d326b0dd3166
uttamkn <uttamkn15@gmail.com>
2026-04-09 00:16:15 +0530

    treesitter bs

--- home/.config/nvim/lua/utmkn/plugins/treesitter.lua
+++ home/.config/nvim/lua/utmkn/plugins/treesitter.lua
@@ -1,59 +1,54 @@
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"python",
+		"c",
+		"cpp",
+		"json",
+		"bash",
+		"lua",
+		"fish",
+	},
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
-	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"windwp/nvim-ts-autotag",
 	},
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-
-		treesitter.setup({
-			ensure_installed = {
-				"c",
-				"cpp",
-				"python",
-				"go",
-				"json",
-				"yaml",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"vimdoc",
-				"fish",
+	opts = {
+		ensure_installed = {
+			"c",
+			"cpp",
+			"python",
+			"go",
+			"json",
+			"yaml",
+			"markdown",
+			"markdown_inline",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"fish",
+		},
+		auto_install = true,
+		highlight = { enable = true, additional_vim_regex_highlighting = false },
+		indent = { enable = true },
+		autotag = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				node_decremental = "<bs>",
 			},
-			auto_install = true,
-			sync_install = false,
-			highlight = {
-				enable = true,
-			},
-			indent = { enable = true },
-			autotag = {
-				enable = true,
-			},
-			disable = function(_, buf) -- Disable for files larger than 100 KB
-				local max_filesize = 100 * 1024 -- 100 KB
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then
-					return true
-				end
-			end,
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-		})
-	end,
+		},
+	},
 }
